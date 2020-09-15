e173ba4336954386f1b825c6e67b71203643b552
Alex Constantinou <a_constantinou@hotmail.co.uk>
2020-09-15 23:28:30 +0100

    print results

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ string_1 = 'Man is distinguished, not only by his reason, but by this singular p
 result_1 = string_to_base64(string_1)
 print(result_1)
 
+print(' ')
+
 string_2 = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 
 result_2 = string_to_base64(string_2)
